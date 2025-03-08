ad78203a960287feedd6a5e22a49c7f06d216312
amt-francis-amos <sugartone45@yahoo.com>
2025-03-08 04:18:55 -0800

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -133,7 +133,7 @@ const Home = () => {
 
       
       <section className="py-16 bg-gray-100">
-  <h2 className="text-3xl font-bold text-center mb-10">What Our Customers Say</h2>
+  <h2 className="text-lg md:text-3xl font-bold text-center mb-10">What Our Customers Say</h2>
   <motion.div
     className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 container mx-auto px-6"
     variants={staggerContainer}
